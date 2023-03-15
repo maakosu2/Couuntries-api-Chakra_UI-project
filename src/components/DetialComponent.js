56c57fb3ced8792dbd97ec7fbe521a8ab7fd82a6
maakosu2 <mandelaakosu@gmail.com>
2023-03-15 07:11:11 +0100

    mobile views css update

--- src/components/DetialComponent.js
+++ src/components/DetialComponent.js
@@ -37,7 +37,7 @@ export default function DetialComponent({topLevelDomain,nativeName,name,populati
    alt='Caffe Latte'
  />
  </Box  >
-   <Stack   py={{sm:28,lg:4}}  px={{sm:"inherit", lg:"5px"}} >
+   <Stack   py={{sm:18,lg:4}}  px={{sm:"inherit", lg:"5px"}} >
     <Box w="350px"   justifyItems="flex-start"
           fontSize={24}
           fontWeight="extrabold"
